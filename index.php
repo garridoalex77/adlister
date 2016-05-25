b00e66860332ab30f6e6f2220b1d41c3d36dd85e
Marcus <mmond21@gmail.com>
2016-05-25 08:35:10 -0500

    adding index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+ ?>
+
+ <!doctype html>
+ <html>
+     <head>
+         <meta charset="utf-8">
+         <meta name="description" content="">
+         <meta name="viewport" content="width=device-width, initial-scale=1">
+         <title>The Adlister</title>
+         <link rel="stylesheet" href="css/style.css">
+         <link rel="author" href="humans.txt">
+     </head>
+     <body>
+         Adlister
+         <script src="js/main.js"></script>
+     </body>
+ </html>
